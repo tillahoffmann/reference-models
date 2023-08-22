e3b44723ca931d3a774bbf6290fa4b97fe0022dc
Till Hoffmann <tillahoffmann@gmail.com>
2023-08-22 19:30:59 -0400

    Add linting and `build` group task.

--- recipe.py
+++ recipe.py
@@ -1,7 +1,12 @@
 from cook import create_task
+from cook.contexts import create_group
 
 
 create_task("requirements", dependencies=["requirements.in", "setup.py"],
             targets=["requirements.txt"], action="pip-compile -v")
-create_task("tests", action="pytest -v --cov=reference_models --cov-report=term-missing "
-            "--cov-fail-under=100")
+
+
+with create_group("build"):
+    create_task("lint", action="flake8")
+    create_task("tests", action="pytest -v --cov=reference_models --cov-report=term-missing "
+                "--cov-fail-under=100")
