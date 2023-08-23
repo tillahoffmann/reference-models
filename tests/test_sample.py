75b130cdc0129dc799624d8e93402959b3e6aafe
Till Hoffmann <tillahoffmann@gmail.com>
2023-08-22 21:27:44 -0400

    Verify there are no unused Stan files.

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import pytest
 from reference_models import COLLECTIONS, sample
 
@@ -14,3 +15,12 @@ SPECS = [
 def test_sample(collection: str, dataset: str, model: str) -> None:
     args = ["--chains=1", "--iter-warmup=10", "--iter-sampling=5", collection, dataset, model]
     sample.__main__(args)
+
+
+def test_no_unused_stan_file() -> None:
+    stan_files = {stan_file.resolve() for stan_file in Path("reference_models").glob("**/*.stan")
+                  if "data" not in stan_file.parts}
+    used_stan_files = {stan_file for experiments in COLLECTIONS.values() for stan_files in
+                       experiments.values() for stan_file in stan_files}
+    unused_stan_files = stan_files - used_stan_files
+    assert not unused_stan_files
