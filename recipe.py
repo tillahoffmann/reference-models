9f21390d6b33e053c78fe294165211e8dfa8cdf7
Till Hoffmann <tillahoffmann@gmail.com>
2023-08-23 15:52:58 -0400

    Add fixed seed and reduce # of samples and chains.

--- recipe.py
+++ recipe.py
@@ -17,6 +17,15 @@ with create_group("build"):
     create_task("tests", action="pytest -v --cov=reference_models --cov-report=term-missing "
                 "--cov-fail-under=100")
 
+if IN_CI:
+    iter_sampling = 5
+    iter_warmup = 10
+    chains = 1
+else:
+    iter_sampling = 100
+    iter_warmup = 500
+    chains = 2
+
 # Generate samples for all models matching the folder structure of the models.
 with create_group("samples"):
     package_path = Path("reference_models").resolve()
@@ -26,10 +35,10 @@ with create_group("samples"):
                 path = stan_file.relative_to(package_path).with_suffix("")
                 output_directory = "samples" / path
                 target = output_directory / "src_info.yaml"
-                action = ["python", "-m", "reference_models.sample", "--summary", "--output",
-                          output_directory]
-                if IN_CI:
-                    action.extend(["--iter-sampling", 5, "--iter-warmup", 10, "--chains", 1])
-                action.extend([collection, dataset, path.name])
+                action = [
+                    "python", "-m", "reference_models.sample", "--summary", "--output",
+                    output_directory, "--seed", 2576, "--iter-sampling", iter_sampling,
+                    "--iter-warmup", iter_warmup, "--chains", chains, collection, dataset, path.name
+                ]
                 create_task(":".join(path.parts), targets=[target], dependencies=[stan_file],
                             action=action)
