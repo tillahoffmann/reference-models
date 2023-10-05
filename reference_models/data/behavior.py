7239866fbb17587677ad6a40534e4588a4984c5b
Till Hoffmann <tillahoffmann@gmail.com>
2023-10-05 17:15:04 -0400

    Order subjects by household.

--- reference_models/data/behavior.py
+++ reference_models/data/behavior.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 from sklearn.preprocessing import LabelEncoder, StandardScaler
+from .util import get_consecutive_labels
 
 
 def _parse_time(time: str) -> float:
@@ -11,8 +12,9 @@ def _parse_time(time: str) -> float:
 
 def load_data() -> dict:
     # Load the data and preprocess as in `Publication script.R` from the supplement of Koster and
-    # McElreath (2017).
-    raw = pd.read_csv(Path(__file__).parent / "behavior.csv")
+    # McElreath (2017). We sort them by household identifier such that individuals belonging to the
+    # same household have consecutive indices.
+    raw = pd.read_csv(Path(__file__).parent / "behavior.csv").sort_values("house")
 
     age, house_size, log_wealth, rain, time = StandardScaler().fit_transform(np.transpose([
         raw.age, raw.dynamic_house_size, np.log(raw.wealth), raw.rain, raw.time.apply(_parse_time),
@@ -22,13 +24,14 @@ def load_data() -> dict:
     X = np.transpose([
         age, age ** 2, log_wealth, sunday, saturday, time, time ** 2, house_size, rain
     ])
+    subject_id = get_consecutive_labels(raw.subject_id, preserve_order=True)
 
     return {
         "N": len(raw),
         "K": raw.response.nunique(),
         "y": LabelEncoder().fit_transform(raw.response) + 1,
-        "N_id": raw.subject_id.max(),
-        "id": raw.subject_id,
+        "N_id": subject_id.max(),
+        "id": subject_id,
         "N_house": raw.house.max(),
         "house_id": raw.house,
         "N_month": raw.month.max(),
